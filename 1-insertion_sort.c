6b80bc2617111fe824a66e8446ccfa7ae45b1e44
Cody <codywalenciak@gmail.com>
2023-12-15 13:43:51 -0600

    Add insertion sort

---
+++ 1-insertion_sort.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * 
+*/
+
+void insertion_sort(listint_t **list)
+{
+	listint_t *sorter = NULL;
+	listint_t *temp = NULL;
+	if (*list != NULL)
+	{
+		sorter = *list;
+	}
+	while (sorter != NULL)
+	{
+		if (sorter->n < sorter->prev->n)
+		{
+			temp = sorter->prev;
+			sorter->next->prev = sorter->prev;
+			temp->next = sorter->next;
+			sorter->next = sorter->prev;
+			sorter->prev = temp->prev;
+			temp->prev = sorter;
+		}
+		else
+		{
+			sorter = sorter->next;
+		}
+	}
+}
