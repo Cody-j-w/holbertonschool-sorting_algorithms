90d7f65a473490de05ada81507c6fc2ff24fa7ad
Cody <codywalenciak@gmail.com>
2023-12-16 14:59:26 -0600

    Removed unused variable

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,7 +8,7 @@
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *sorter = NULL, *temp = *list, *next = NULL, *prev = NULL;
+	listint_t *sorter = NULL, *temp = *list, *prev = NULL;
 	
 	while (temp != NULL)
 	{
@@ -29,6 +29,7 @@ void insertion_sort_list(listint_t **list)
 				}
 			}
 		}
+		temp = temp->next;
 	}
 	/**
 	 * while (temp != NULL)
