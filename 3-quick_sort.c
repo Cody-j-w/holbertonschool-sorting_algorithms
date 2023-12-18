bfe67e55399170d4167121428882ccf2df571576
Cody <codywalenciak@gmail.com>
2023-12-18 13:27:12 -0600

    Testing methods of printing array

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -24,7 +24,6 @@ int partition(int *array, int low, int high, size_t size)
 			n++;
 			array[i] = array[n];
 			array[n] = temp;
-			print_array(array, size);
 		}
 	}
 	temp = array[n + 1];
@@ -40,6 +39,7 @@ void sort(int *arr, int low, int high, size_t size)
 	if (low < high)
 	{
 		set_partition = partition(arr, low, high, size);
+		print_array(arr, size);
 		sort(arr, low, set_partition - 1, size);
 		sort(arr, set_partition + 1, high, size);
 	}
