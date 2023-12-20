af5fe5f1364035888d13b8c10b8d05d356233408
Cody <codywalenciak@gmail.com>
2023-12-20 11:24:20 -0600

    Reverting to a previous test

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -4,7 +4,13 @@
 #include "sort.h"
 
 /**
+ * partition - partitions an array or subarray
+ * @array: the array or subarray being partitioned
+ * @low: the first index
+ * @high: the last index
+ * @size: the size of the array
  * 
+ * Return: the index of the partition
 */
 
 
@@ -21,16 +27,25 @@ int partition(int *array, int low, int high, size_t size)
 			temp = array[i];
 			array[i] = array[j];
 			array[j] = temp;
-			print_array(array, size);
 		}
 	}
-
-	temp = array[high];
-	array[high] = array[i + 1];
-	array[i + 1] = temp;
-
+	if (array[high] != array[i+1])
+	{
+		temp = array[high];
+		array[high] = array[i + 1];
+		array[i + 1] = temp;
+		print_array(array, size);
+	}
 	return (i + 1);
 }
+
+/**
+ * sort - recursively sorts the array, using partition to form subarrays
+ * @array: the array, or subarray, being sorted
+ * @low: the lowest index of the current array
+ * @high: the highest index of the current array
+ * @size: the size of the array
+*/
 
 void sort(int *array, int low, int high, size_t size)
 {
@@ -44,6 +59,12 @@ void sort(int *array, int low, int high, size_t size)
 		sort(array, index + 1, high, size);
 	}
 }
+
+/**
+ * quick_sort - calls sort to recursively sort an array
+ * @array: the array to be sorted
+ * @size: the size of the array
+*/
 
 void quick_sort(int *arr, size_t size)
 {
