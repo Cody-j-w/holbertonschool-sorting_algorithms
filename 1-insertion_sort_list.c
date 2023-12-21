c3e9f888777bf3af3412d99df4ccd99ecfc01a99
Cody <codywalenciak@gmail.com>
2023-12-21 14:47:45 -0600

    Add blank line after declarations

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,6 +10,7 @@
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *temp = *list, *next = NULL, *prev = NULL;
+	
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 	while (temp != NULL)
