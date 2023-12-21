57fb25498dfd8b3246e3ca5ddcaa6220fbac95dc
Cody <codywalenciak@gmail.com>
2023-12-21 14:44:23 -0600

    Style and documentation

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -3,34 +3,37 @@
 #include "sort.h"
 
 /**
- * 
+ * insertion_sort_list - use insertion sort to sort a linked list
+ * @list: the list being sorted
 */
 
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *temp = *list, *next = NULL, *prev = NULL;
-	
-	while (temp != NULL)
+	if (*list != NULL)
 	{
-		next = temp->next;
-		prev = temp->prev;
-		while (prev != NULL && temp->n < prev->n)
+		while (temp != NULL)
 		{
-			if (prev->prev != NULL)
-				prev->prev->next = temp;
-			prev->next = temp->next;
-			temp->next = prev;
-			temp->prev = prev->prev;
-			prev->prev = temp;
-			if (prev->next != NULL)
-				prev->next->prev = prev;
-			if (temp->prev == NULL)
-				*list = temp;
-			
+			next = temp->next;
 			prev = temp->prev;
+			while (prev != NULL && temp->n < prev->n)
+			{
+				if (prev->prev != NULL)
+					prev->prev->next = temp;
+				prev->next = temp->next;
+				temp->next = prev;
+				temp->prev = prev->prev;
+				prev->prev = temp;
+				if (prev->next != NULL)
+					prev->next->prev = prev;
+				if (temp->prev == NULL)
+					*list = temp;
+				
+				prev = temp->prev;
 
-			print_list(*list);
+				print_list(*list);
+			}
+			temp = next;
 		}
-		temp = next;
 	}
 }
